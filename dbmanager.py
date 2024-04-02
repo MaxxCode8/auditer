14ab33469fffb5c56f6bbfcf441e147aff843754
foolmalhar <inficuriosity00@gmail.com>
2024-04-02 11:49:29 +0530

    🍦 version 1

--- dbmanager.py
+++ dbmanager.py
@@ -1,5 +1,6 @@
-import pandas as pd
+from datetime import datetime, timedelta
 import os
+import pandas as pd
 
 class DatabaseManager:
     
@@ -39,15 +40,39 @@ class DatabaseManager:
     ####################################################################################### Daily Audits DB
     
     def read_daily_audits(self):
-        if not os.path.exists(self.daily_audits_path):
-            self.create_empty_audit_file(self.daily_audits_path)
-        return pd.read_csv(self.daily_audits_path)
+        try:
+            df = pd.read_csv(self.daily_audits_path)
+            today_audits = df[df['date'] == pd.to_datetime('today').strftime('%Y-%m-%d')]
+            return today_audits
+        except Exception as e:
+            print(f"Error reading daily audits: {e}")
 
-    def write_daily_audits(self, df):
+    def write_daily_audits(self, new_audit):
+        # APPEND the new audit directly to the CSV file
+        with open(self.daily_audits_path, 'a') as file:
+            file.write(f"{new_audit['priority']},{new_audit['description']},{new_audit['time_spent']},{new_audit['start_time']},{new_audit['end_time']},{new_audit['date']}\n")
+
+    def update_daily_audits(self, audit):
+        # Read the entire CSV file into a DataFrame
+        df = pd.read_csv(self.daily_audits_path)
+        # Update the specific row
+        df.loc[(df['date'] == audit['date']) & (df['start_time'] == audit['start_time']), ['end_time', 'time_spent']] = [audit['end_time'], audit['time_spent']]
+        # Write the updated DataFrame back to the CSV file
         df.to_csv(self.daily_audits_path, index=False)
 
+    def delete_audit_entry(self, start_ts):
+        df = pd.read_csv(self.daily_audits_path)
+        df = df[df['start_time'] != start_ts]
+        df.to_csv(self.daily_audits_path, index=False)
+        
     ####################################################################################### Weekly Audits DB
-
+    def fetch_last_seven(self):
+        df = pd.read_csv(self.daily_audits_path)
+        df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+        seven_days_ago = datetime.now() - timedelta(days=7)
+        filtered_df = df[df['date'] >= seven_days_ago]
+        return filtered_df
+    
     def read_weekly_audits(self):
         if not os.path.exists(self.weekly_audits_path):
             self.create_empty_audit_file(self.weekly_audits_path)
--- stapp.py
+++ stapp.py
@@ -1,9 +1,12 @@
 import datetime
-import streamlit as st
-from dbmanager import DatabaseManager
-import pandas as pd
-from datetime import datetime
 import time
+from datetime import datetime
+import numpy as np
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from dbmanager import DatabaseManager
+
 # Today's Date:
 today = datetime.today().strftime("%A : %d/%m/%y")
 
@@ -49,8 +52,6 @@ def edit_priority_list():
             st.success(f"{new_priority_item} added to the priority list.")
             # Update the session state with the new list
             st.session_state.priority_list = db_manager.read_priority_list()
-            # Sort the priority list
-            st.session_state.priority_list.sort()
             # Reload the page to show the updated list
             st.rerun()
         else:
@@ -68,37 +69,31 @@ def update_daily_audits():
     # Form for adding new audits
     with st.form(key='add_audit_form'):
         priority = st.selectbox("Select Priority", priority_options)
-        description = st.text_input("Description")
-        submit_button = st.form_submit_button("Start🚀")
+        description = st.text_input("Specify Task (Aspect)")
+        start_button = st.form_submit_button("Start🚀")
 
-        if submit_button:
-            # daily_audit_columns = ['priority_cat_str', 'description_str', 'time_spent_timestampsubstration', 'start_time', 'end_time', "date"]
-            # Append the new audit to the daily audits DataFrame
-            new_audit = {
-                'priority': priority, 'description': description, 
-                'time_spent': "N0ne", 'start_time': time.time(),    # current timestamp
-                'end_time': "N0ne",
-                'date': pd.to_datetime('today').strftime('%Y-%m-%d')}
-
-            new_audit_df = pd.DataFrame([new_audit])
-
-            if st.session_state.daily_audits_df.empty:
-                st.session_state.daily_audits_df = new_audit_df
-            else:
-                st.session_state.daily_audits_df = pd.concat([st.session_state.daily_audits_df, new_audit_df], ignore_index=True)
-
-            db_manager.write_daily_audits(st.session_state.daily_audits_df)
-            st.success("Audit added successfully.")
-            # Update the session state with the new list
-            st.session_state.daily_audits_df = db_manager.read_daily_audits()
-            # Reload the page to show the updated list
-            st.rerun() # REFRESEHs the page
+        if start_button:
+                # daily_audit_columns = ['priority_cat_str', 'description_str', 'time_spent_timestampsubstration', 'start_time', 'end_time', "date"]
+                # Append the new audit to the daily audits DataFrame
+                new_audit = {
+                    'priority': priority, 'description': '"'+description+'"', 
+                    'time_spent': "N0ne", 'start_time': time.time(),    # current timestamp
+                    'end_time': "N0ne",
+                    'date': pd.to_datetime('today').strftime('%Y-%m-%d')
+                    }
+                
+                db_manager.write_daily_audits(new_audit)
+                st.success("Audit added successfully.")
+                # Update the session state with the new list
+                st.session_state.daily_audits_df = db_manager.read_daily_audits()
+                # Reload the page to show the updated list
+                st.rerun() # REFRESEHs the page
 
     # Display existing audits
     if st.session_state.daily_audits_df.empty:
         st.info("No audits found.")
     else:   
-        st.markdown("### Priority🧘‍♀️                  Description📃            Time Spent ⏲")
+        st.markdown("### Priority🧘‍♀️         Description📃             Time Spent ⏲")
         for index, row in st.session_state.daily_audits_df.iterrows():
             col1, col2, col3, col4 = st.columns([2,3,2,1])
             with col1:
@@ -106,7 +101,6 @@ def update_daily_audits():
             with col2:
                 st.markdown(f"##### :green[{row['description']}]")
             with col3:
-                # st.write(row['end_time'])
                 if row['end_time'] == "N0ne":
                     taskend = st.button("✅ :blue[**End Task**]", key=index)
                     if taskend:
@@ -116,16 +110,17 @@ def update_daily_audits():
                         time_spent = round((end_time-start_time) / 3600,2)
                         # Update the session dataframe
                         st.session_state.daily_audits_df.loc[index, "time_spent"] = time_spent
+                        db_manager.update_daily_audits(st.session_state.daily_audits_df.loc[index])
                         st.rerun()
-                        # Session dataframe still not updated in the csv
                 else:                    
                     st.markdown(f"##### :orange[{row['time_spent']} hours]")
             with col4:
                 if st.button(f"❌", key=f"delete_audit_{index}"):
                     # Delete the audit from the DataFrame
+                    start_time = float(st.session_state.daily_audits_df.loc[index, "start_time"])
                     st.session_state.daily_audits_df.drop(index, inplace=True)
-                    db_manager.write_daily_audits(st.session_state.daily_audits_df)
-                    st.success(f"Audit {index} deleted successfully.")
+                    db_manager.delete_audit_entry(start_time)    
+                    st.success("Entry Deleted")
                     # Update the session state with the new list
                     st.session_state.daily_audits_df = db_manager.read_daily_audits()
                     # Reload the page to show the updated list
@@ -134,14 +129,104 @@ def update_daily_audits():
 
 # Function to display weekly audit report
 def display_weekly_audit_report():
-    # Example: Read the weekly audits and display them
-    weekly_audits = db_manager.read_weekly_audits()
-    st.write(weekly_audits)
+
+    # if st.button("Give Me Last 7 Day Audit 🧮"):
+    #     df7 = db_manager.fetch_last_seven()
+    #     grouped_df = df7.groupby('priority')['time_spent'].sum()
+
+    #     # Sort the data by total time spent
+    #     sorted_df = grouped_df.sort_values(ascending=True)
+
+    #     # Create a figure and a set of subplots
+    #     fig, ax = plt.subplots(figsize=(16, 12))
+
+    #     # Define a list of colors for each bar
+    #     colors = plt.cm.viridis(np.linspace(0, 1, len(sorted_df)))
+
+    #     # Plot the horizontal histogram on the first subplot
+    #     bars = ax.barh(sorted_df.index, sorted_df.values, color=colors)
+
+    #     # Display the total time spent on top of each bar in "X hrs : Y min" format
+    #     for bar in bars:
+    #         width = bar.get_width()
+    #         hours = int(width)
+    #         minutes = int((width - hours) * 60)
+    #         ax.text(width, bar.get_y() + bar.get_height()/2, f'{hours} hrs : {minutes} min', va='center', ha='left',
+    #                 fontsize=15, fontweight='bold', color='white', bbox=dict(facecolor='black', alpha=0.8, boxstyle='round,pad=0.3'))
+
+    #     ax.set_ylabel('Priority', fontsize=15, fontweight='bold')
+    #     ax.set_xlabel('Total Time Spent', fontsize=15, fontweight='bold')
+    #     ax.set_title('Total Time Spent per Priority', fontsize=16, fontweight='bold')
+    #     ax.set_yticklabels(sorted_df.index, fontsize=15) # Adjust y-axis labels for better readability
+
+    #     # Set a gradient background
+    #     ax.set_facecolor('lightgray')
+    #     ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
+
+    #     # Display the figure in Streamlit
+    #     st.pyplot(fig)
+        
+
+    if st.button("Give Me Last 7 Day Audit 🧮"):
+        df7 = db_manager.fetch_last_seven()
+        grouped_df = df7.groupby('priority')['time_spent'].sum()
+
+        # Sort the data by total time spent
+        sorted_df = grouped_df.sort_values(ascending=False)
+
+        # Create a figure and a set of subplots
+        fig, ax = plt.subplots(figsize=(15, 11))
+
+        # Define a list of colors for each bar
+        colors = plt.cm.viridis(np.linspace(0, 1, len(sorted_df)))
+
+        # Plot the histogram on the first subplot
+        bars = ax.bar(sorted_df.index, sorted_df.values, color=colors)
+
+        # Display the total time spent on top of each bar in "X hrs : Y min" format
+        for bar in bars:
+            height = bar.get_height()
+            hours = int(height)
+            minutes = int((height - hours) * 60)
+            ax.text(bar.get_x() + bar.get_width()/2, height, f'{hours} hrs : {minutes} min', ha='center', va='bottom',
+                    fontsize=15, fontweight='bold', color='white', bbox=dict(facecolor='black', alpha=0.8, boxstyle='round,pad=0.5'))
+
+        ax.set_xlabel('Priority', fontsize=15, fontweight='bold')
+        ax.set_ylabel('Total Time Spent', fontsize=15, fontweight='bold')
+        ax.set_title('Total Time Spent per Priority', fontsize=20, fontweight='bold')
+        
+        # Set the x-ticks to match the number of labels
+        ax.set_xticks(range(len(sorted_df.index)))
+        ax.set_xticklabels(sorted_df.index, rotation=35, fontsize=14, fontweight='bold') # Rotate x-axis labels for better readability
+
+        # Set a gradient background
+        ax.set_facecolor('lightgray')
+        ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
+
+        # Display the figure in Streamlit
+        st.pyplot(fig)
+
+
+
+        @st.cache_data
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+
+        csv = convert_df(pd.read_csv('dailyaudits.csv'))
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'daily_audits_till_{datetime.today().strftime("%y-%m-%d")}.csv',
+            mime='text/csv',
+        )
 
 # Main Streamlit app
 def main():
-    st.markdown("# Auditer...")
+    st.markdown("# Self Audit is Self Transformation")
     st.markdown(f"#### :orange[*{today}*]")
+    st.write("")
     st.write("")
     st.write("")
     tab1, tab2, tab3 = st.tabs(["Edit Priority List", "Daily Audits", "Weekly Audit Report"])
@@ -156,3 +241,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# - Manully set Start and End Task Time ⭐⭐⭐⭐⭐
+# - Specific Entry Deletion ISSUE 🔴 : Deletes Entire Database 
